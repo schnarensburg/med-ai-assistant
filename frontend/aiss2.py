b44de150b09bb576b4d6f1e25f76d51e7bcff98a
alimacarual <ugnvd@student.kit.edu>
2025-06-28 15:46:04 +0200

    Deleted files

--- frontend/aiss2.py
+++ frontend/aiss2.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title("Hello world")
+st.title("My Hello world")
 st.header("_Streamlit_ is :blue[cool] :sunglasses:")
 st.header("This is a header with a divider", divider="gray")
